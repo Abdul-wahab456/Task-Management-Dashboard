518bb4b46b4f13ba929e0276d2563936f1b9f7e4
Abdul wahab <Abdulwahab2009012@gmail.com>
2025-09-10 19:34:54 +0500

    main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
 import Dashboard from "../src/pages/Dashboard";
 import Setting from "../src/pages/Settings";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -15,25 +13,3 @@ createRoot(document.getElementById("root")).render(
     </BrowserRouter>
   </StrictMode>
 );
-=======
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Dashboard from '../pages/Dashboard';
-import Setting from '../pages/Settings';
-import Task from '../pages/Tasks';
-import {BrowserRouter,Routes,Route,useNavigate } from "react-router-dom";
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Dashboard />}/>
-      <Route path="/setting" element={<Setting />}/>
-      <Route path="/task" element={<Task />}/>
-    </Routes>
-    </BrowserRouter>
-  </StrictMode>
-)
-
-
->>>>>>> 64a84887794e83e783f4938dd85f774ea420a0f3
